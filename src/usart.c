b05f9d60d7360d650aa27475112b8e01f1ab8337
Kalyan Sriram <coder.kalyan@gmail.com>
2020-10-12 09:21:05 -0700

    usart: change send payload to char and add puts helper methods

--- src/usart.c
+++ src/usart.c
@@ -1,5 +1,5 @@
-#include "gpio.h"
-#include "usart.h"
+#include <avrl/gpio.h>
+#include <avrl/usart.h>
 
 void usart_set_baudrate(uint8_t usart, uint32_t baud)
 {
@@ -37,7 +37,7 @@ void usart_set_mode(uint8_t usart, uint8_t mode)
     USART_MR(usart) |= (mode << USART_MODE_BIT_BASE);
 }
 
-void usart_send(uint8_t usart, uint8_t data)
+void usart_send(uint8_t usart, char data)
 {
     USART_TDR(usart) = data;
 }
@@ -47,13 +47,13 @@ void usart_wait_send_ready(uint8_t usart)
     while ((USART_TSR(usart) & (1 << USART_TRANSMIT_BIT_BASE)) == 0);
 }
 
-void usart_send_blocking(uint8_t usart, uint8_t data)
+void usart_send_blocking(uint8_t usart, char data)
 {
     usart_wait_send_ready(usart);
     usart_send(usart, data);
 }
 
-uint8_t usart_read(uint8_t usart)
+char usart_read(uint8_t usart)
 {
     uint8_t data;
     data = USART_RDR(usart);
@@ -65,7 +65,7 @@ void usart_wait_recv_ready(uint8_t usart)
     while ((USART_RSR(usart) & (1 << USART_RECEIVE_BIT_BASE)) == 0);
 }
 
-uint8_t usart_read_blocking(uint8_t usart)
+char usart_read_blocking(uint8_t usart)
 {
     usart_wait_recv_ready(usart);
     return usart_read(usart);
@@ -79,4 +79,18 @@ void usart_enable_rx_interrupt(uint8_t usart)
 void usart_disable_rx_interrupt(uint8_t usart)
 {
     USART_RIR(usart) &= ~(1 << USART_RX_INTERRUPT);
+}
+
+void usart_puts(uint8_t usart, char *string)
+{
+    while (*string) {
+        usart_send_blocking(usart, *string);
+        string++;
+    }
+}
+
+void usart_putln(uint8_t usart, char *string)
+{
+    usart_puts(usart, string);
+    usart_puts(usart, "\r\n");
 }
