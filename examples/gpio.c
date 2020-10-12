3daddc01b030ae22444f1e78731a38c6296388b6
bbworld1 <vwangsf@gmail.com>
2020-10-12 16:11:07 -0700

    Add other methods in gpio example

--- examples/gpio.c
+++ examples/gpio.c
@@ -7,7 +7,7 @@
 
 // Define port and pin constants
 const int GPIO_PORT = GPIOB;
-const int GPIO_PIN = GPIO7;
+const int GPIO_PIN = GPIO5;
 
 int main() {
     // Setup the pin in OUTPUT mode
@@ -25,4 +25,28 @@ int main() {
         // wait 1000ms (busy wait)
         _delay_ms(1000);
     }
+
+    // // The same thing, but with the toggle() function
+    // while (1) {
+    //     // toggle GPIO pin (led toggle)
+    //     gpio_toggle(GPIO_PORT, GPIO_PIN);
+    //
+    //     // wait 1000ms (busy wait)
+    //     _delay_ms(1000);
+    // }
+
+    // // The same thing, but with the write() function
+    // while (1) {
+    //     // write gpio HI (led on)
+    //     gpio_write(GPIO_PORT, GPIO_PIN, GPIO_HIGH);
+    //
+    //     // wait 1000ms (busy wait)
+    //     _delay_ms(1000);
+    //
+    //     // write gpio LO (led off)
+    //     gpio_write(GPIO_PORT, GPIO_PIN, GPIO_LOW);
+    //
+    //     // wait 1000ms (busy wait)
+    //     _delay_ms(1000);
+    // }
 }
